6b243fb228e2251aa5ed3ff95397e65746a194fb
juicerq <julio.cerqueiira@gmail.com>
2024-02-04 13:10:28 -0300

    melhoria: visual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,49 @@
-import { unstable_noStore as noStore } from "next/cache";
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
 export default async function Home() {
-  noStore();
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center text-black">
-      <div className="bg-black/10 p-8">
-        <div className="grid w-[500px] grid-cols-8 gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Card className="">
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>Agende seu horário</CardTitle>
+              <CardDescription>
+                Clique em um dia para agendar seu horário
+              </CardDescription>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex items-center gap-1">
+                <div className="h-2 w-2 rounded-md bg-emerald-500" />
+                <p className="text-muted-foreground text-xs">Disponível</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <div className="h-2 w-2 rounded-md bg-red-500" />
+                <p className="text-muted-foreground text-xs">Preenchido</p>
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="flex w-[600px] flex-wrap gap-4">
           {Array.from({ length: 31 }).map((_, i) => (
-            <div className="col-span-1 h-10 w-10 rounded-md bg-white text-center">
-              Hi
+            <div
+              key={i}
+              className={`hover:bg-secondary ${i % 2 ? "text-emerald-600" : "text-red-600"} flex h-10 w-10 cursor-pointer items-center justify-center rounded-md border text-sm transition-all duration-300 active:scale-110`}
+            >
+              {i + 1}
             </div>
           ))}
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
