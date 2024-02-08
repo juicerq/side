19d531fb483ad0e11750bdd0305c8f593abd5bcb
juicerq <julio.cerqueiira@gmail.com>
2024-02-07 22:45:11 -0300

    componente de schedule

---
+++ src/app/components/Schedule.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Account } from "./Account";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+export const Schedule = () => {
+  return (
+    <div>
+      <Account />
+      <Card className="">
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>Agende seu horário</CardTitle>
+              <CardDescription>
+                Clique em um dia para agendar seu horário
+              </CardDescription>
+            </div>
+
+            <div className="flex flex-col justify-between">
+              <div className="flex items-center gap-1">
+                <div className="h-2 w-2 rounded-md bg-emerald-500" />
+                <p className="text-xs text-muted-foreground">Disponível</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <div className="h-2 w-2 rounded-md bg-red-500" />
+                <p className="text-xs text-muted-foreground">Preenchido</p>
+              </div>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="flex w-[600px] flex-wrap gap-4">
+          {Array.from({ length: 31 }).map((_, i) => (
+            <div
+              key={i}
+              className={`hover:bg-secondary ${i % 2 ? "cursor-pointer text-emerald-500 hover:rounded-lg" : "pointer-events-none text-red-500"} flex h-10 w-10 items-center justify-center rounded-md border text-sm transition-all duration-300 active:scale-110`}
+            >
+              {i + 1}
+            </div>
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
