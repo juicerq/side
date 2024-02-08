b4c1033db7b815f5685ded6485b8969f8949aa68
juicerq <julio.cerqueiira@gmail.com>
2024-02-07 22:45:23 -0300

    pagina de login e registro de usuario

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginUser from "../components/LoginUser";
+
+export const Login = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <LoginUser />
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateAccountForm from "../components/CreateAccountForm";
+
+const Register = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <CreateAccountForm />
+    </div>
+  );
+};
+
+export default Register;
