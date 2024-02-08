a065cd9846df0ab23bc6024128c26cf322b2cfe0
juicerq <julio.cerqueiira@gmail.com>
2024-02-08 20:55:53 -0300

    getuser  dont return array

--- src/server/api/utils/getUser.ts
+++ src/server/api/utils/getUser.ts
@@ -1,11 +1,16 @@
 import { db } from "@/server/db";
 import { users } from "@/server/db/schema";
 import { eq } from "drizzle-orm";
+import { takeUniqueOrThrow } from "./DrizzleUtils";
 
 export async function getUser(userUuid: string | null) {
   if (!userUuid) return null;
 
-  const user = await db.select().from(users).where(eq(users.uuid, userUuid));
+  const user = await db
+    .select()
+    .from(users)
+    .where(eq(users.uuid, userUuid))
+    .then(takeUniqueOrThrow);
 
-  return { user: user[0] };
+  return { user };
 }
