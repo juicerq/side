0289954a3d0acbd5e28072730d048d7bc5ec78e0
juicerq <julio.cerqueiira@gmail.com>
2024-02-08 21:13:09 -0300

    muda login para default function

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import LoginUser from "../components/LoginUser";
 
-export const Login = () => {
+export default function Login() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <LoginUser />
     </div>
   );
-};
-
-export default Login;
+}
