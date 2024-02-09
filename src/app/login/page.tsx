c7c7731633f198cc05000133a6dd95a278521ba2
juicerq <julio.cerqueiira@gmail.com>
2024-02-08 21:20:09 -0300

    rename page functions

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LoginUser from "../components/LoginUser";
 
-export default function Login() {
+export default function LoginPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <LoginUser />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CreateAccountForm from "../components/CreateAccountForm";
 
-export default function Register() {
+export default function RegisterPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <CreateAccountForm />
