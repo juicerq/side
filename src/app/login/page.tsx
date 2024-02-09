b355ec46c7d89c12a80102f031fae73db46f1096
juicerq <julio.cerqueiira@gmail.com>
2024-02-09 19:17:14 -0300

    teste

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,12 @@
+import { api } from "@/trpc/react";
 import { LoginUser } from "../components/LoginUser";
 
 export default function LoginPage() {
+  const { data } = api.user.hello.useQuery({ text: "funciona" });
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
+      <p>{data?.greeting}</p>
       <LoginUser />
     </div>
   );
