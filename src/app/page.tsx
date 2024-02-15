453c52736ca2843f92cc50199ed9f2e36dc4f66e
juicerq <julio.cerqueiira@gmail.com>
2024-02-15 19:31:44 -0300

    add loading animation to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { api } from "@/trpc/react";
+import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { data: isLogged } = api.user.verify.useQuery();
+  const { data: isLogged, isLoading } = api.user.verify.useQuery();
   const router = useRouter();
 
   if (isLogged === false) router.push("/login");
 
   if (isLogged === true) router.push("/schedule");
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      {isLoading && <Loader2 className="h-12 w-12 animate-spin" />}
+    </div>
+  );
 }
