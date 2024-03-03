72e3045669f50a4f2ef7c24a740fcc9b5ab5cf4c
juicerq <julio.cerqueiira@gmail.com>
2024-03-03 13:30:15 -0300

    fix of fix

--- src/app/schedule/components/HourInfo.tsx
+++ src/app/schedule/components/HourInfo.tsx
@@ -9,17 +9,16 @@ export default function HourInfo({ hour }: HourInfoProps) {
   return (
     <div className="flex flex-wrap bg-primary-foreground p-4 rounded-lg mt-4 gap-6">
       {hour.map((hour) => (
-        <div key={hour.uuid} className="flex items-center gap-2">
-          <Button
-            variant="secondary"
-            className="flex bg-background items-center gap-2"
-          >
-            <div
-              className={`h-2 w-2 rounded-md ${hour.available ? "bg-emerald-500" : "bg-red-500"} `}
-            />
-            <p className="text-xs">{hour.hour}</p>
-          </Button>
-        </div>
+        <Button
+          variant="secondary"
+          key={hour.uuid}
+          className="flex bg-background items-center gap-2"
+        >
+          <div
+            className={`h-2 w-2 rounded-md ${hour.available ? "bg-emerald-500" : "bg-red-500"} `}
+          />
+          <p className="text-xs">{hour.hour}</p>
+        </Button>
       ))}
     </div>
   );
