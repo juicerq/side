b5380ae6ee89c97037df9aa3c5b315f4b27957c6
juicerq <julio.cerqueiira@gmail.com>
2024-03-06 20:26:54 -0300

    i hate vercel

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ const config = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/prefer-optional-chain": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
